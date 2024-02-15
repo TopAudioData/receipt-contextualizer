bafbb13965dd364ed2f4128e9d97f75b24967417
Leonard Kamps <leonardkamps@MacBook-Air-von-Leo.local>
2024-02-15 12:35:38 +0100

    Scraped categories, refactored further, started categorizing on donut scanned items, kind of random commit

--- scripts/refactored.py
+++ scripts/refactored.py
@@ -1,11 +1,10 @@
+from mistralai.client import MistralClient
+from ast import literal_eval
+
 def get_embeddings_by_chunks(data, chunk_size, api_key):
     '''
     Returns embeddings for data as a list of arrays.
     '''
-
-    from mistralai.client import MistralClient
-    from ast import literal_eval
-
     client = MistralClient(api_key)
 
     chunks = [data[x : x + chunk_size] for x in range(0, len(data), chunk_size)]
