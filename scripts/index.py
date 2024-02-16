2c81458eac3fa9e165f229a0f4a6b6eecb69a562
Leonard Kamps <leonardkamps@MacBook-Air-von-Leo.fritz.box>
2024-02-16 17:26:23 +0100

    zwischenspeichern

--- scripts/index.py
+++ scripts/index.py
@@ -2,4 +2,5 @@ from refactored import get_embeddings_by_chunks
 import processing as p
 import pandas as pd
 
-product_json = p.process_item('GENUSS SCHEIBEN')
+product_json = p.process_item('VKORN SONNENBATZ')
+print(product_json)
