34b7b51e69fa7fb02dd6b074c130607149a38888
Leonard Kamps <leonardkamps@MacBook-Air-von-Leo.local>
2024-02-27 16:27:28 +0100

    After rehearsal

--- production/pages/visualization.py
+++ production/pages/visualization.py
@@ -11,6 +11,7 @@ import json
 from sympy import marcumq
 
 import process_llm as llm
+import database as db
 
 # Page config
 st.set_page_config(
@@ -53,10 +54,8 @@ plot_products.drop(['price','image','x_embeds_tsne','y_embeds_tsne'],axis=1,inpl
 categories = llm.get_rewe_categories()
 
 
-intro, prompting, embeddings = st.tabs(['Intro', 'LLM as classifier model', 'LLM text embeddings'])
+prompting, embeddings = st.tabs(['LLM as classifier model', 'LLM text embeddings'])
 
-with intro:
-    st.image('https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExN21neXU2Y2JlYjRoN29xb2Q2N3RxZGVhaWpoZDd6cDJuZmxqNmFzaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WRQBXSCnEFJIuxktnw/giphy.gif')
 
 with prompting:
     # 1st step
@@ -124,7 +123,8 @@ with prompting:
                 response = mistral_api(query, categories)
                 st.table(pd.DataFrame(response).T)
             else:
-                st.info('👈 Enter an item from a receipt.')
+                #st.info('👈 Enter an item from a receipt.')
+                pass
         
 
     # Display categories used for LLM prompt
@@ -152,6 +152,56 @@ with prompting:
         To understand why this works, take a look at embeddings. This also explains some errors,
         when a product is placed in a different category by the model than the supermarked did.
         """)
+    
+    with st.expander('Augmented data'):
+        #Get receipts data from database
+        df = db.data()
+        column_names = {
+            'id_pk':'ID',
+            'receipt_id':'Receipt',
+            'receipt_date':'Date',
+            'price':'Price',
+            'product_abbr':'Name on receipt',
+            'product_name':'Name',
+            'category_main':'Category',
+            'category_sub':'Kind',
+            'embedding':'Semantic coordinates'
+        }
+
+        # For prototype: insert dates
+        receipt_ids =   ['Rewe_1.jpg', 'Rewe_2.jpg', 'Rewe_3.jpg', 'Rewe_4.jpg', 'Rewe_5.jpg', 'Rewe_6.jpg', 'Rewe_7.jpg', 'Rewe_8.jpg', 'Rewe_9.jpg', 'Rewe_10.jpg', 'Rewe_11.jpg', 'Rewe_12.jpg', 'Rewe_13.jpg', 'Rewe_14.jpg']
+        receipt_dates = ['20.01.2024', '12.12.2023', '30.12.2023', '13.12.2023', '08.11.2023', '11.11.2023', '18.11.2023', '11.11.2023', '07.11.2023', '22.01.2024',  '04.12.2023',  '09.02.2024',  '20.12.2023',  '03.01.2024']
+
+        df_dates = pd.DataFrame(zip(receipt_ids, receipt_dates), columns=['receipt_id', 'receipt_date'])
+        df_dates.receipt_date = pd.to_datetime(df_dates.receipt_date, dayfirst=True)
+        df = df.join(df_dates.set_index('receipt_id'), on='receipt_id')
+
+
+
+
+        # Show all data and edit data in expander
+
+        full_data = st.toggle('Show :receipt::nerd_face::bar_chart: generated data')
+        if full_data:
+            # Show AI generated data
+            st.dataframe(df.rename(columns=column_names).sort_values(by='Date', ascending=False)
+                        [['Date', 'Name on receipt', 'Price', 'Name', 'Category', 'Kind']], # TODO: add back in 'Receipt' after presentation
+                        column_config={
+                            'Date': st.column_config.DateColumn(format='DD.MM.YYYY'),
+                            'Price': st.column_config.NumberColumn(format='%.2f €')},
+                            height=600,
+                            hide_index=True)
+            # TODO: Embed edited entries and overwrite entry in db
+            #st.button('Submit Edits', type='primary')
+        else:
+            # Show prettified dataframe
+            st.dataframe(df.rename(columns=column_names).sort_values(by='Date', ascending=False)
+                        [['Date', 'Name on receipt', 'Price']], # TODO: add back in 'Receipt' after presentation
+                        column_config={
+                            'Date': st.column_config.DateColumn(format='DD.MM.YYYY'),
+                            'Price': st.column_config.NumberColumn(format='%.2f €')},
+                            height=600,
+                            hide_index=True)
 
 
 with embeddings:
@@ -159,52 +209,60 @@ with embeddings:
     st.subheader('Part 2: LLM text embeddings')
 
 
- 
+    example, tsne = st.tabs(['Embeddings in general', 'Semantic space'])
 
-    # This function shall plot the embedded products by category 
-    # - the user can choose which categories shall be plotted
+    with example:
+        #formula = st.container(height=400, border=False)
+        #_, center, _ = formula.columns([1,2,1])
+        #with formula:
+        st.markdown('# \n# ')
+        st.latex('\mathrm{king} - \mathrm{man} + \mathrm{woman} = \mathrm{queen}')
 
-    # Make a list of all categories and add an option to use all categories
-    prod_list = rewe_products.category.unique().tolist()
-    keys = [num for num in range(17)] # produce a list of unique keys for each category
-    prod_list.insert(0,'ALLE')
-    categories = st.multiselect('Multiselect', prod_list) # creates the selection object for the categories
-    if "ALLE" in categories:
-        categories = prod_list
+    with tsne:
+        # This function shall plot the embedded products by category 
+        # - the user can choose which categories shall be plotted
 
-    # create a dictionary that contains the parts of the rewe_products DF that correspond to the chosen category
-    dfs = {cat: rewe_products[rewe_products["category"] == cat] for cat in categories}
+        # Make a list of all categories and add an option to use all categories
+        prod_list = rewe_products.category.unique().tolist()
+        keys = [num for num in range(17)] # produce a list of unique keys for each category
+        prod_list.insert(0,'ALLE')
+        categories = st.multiselect('Multiselect', prod_list, default='ALLE', label_visibility='collapsed') # creates the selection object for the categories
+        if "ALLE" in categories:
+            categories = prod_list
 
-    # use a color_scale to make sure all categories are ploted in unique colors
-    color_scale = px.colors.qualitative.Light24_r
+        # create a dictionary that contains the parts of the rewe_products DF that correspond to the chosen category
+        dfs = {cat: rewe_products[rewe_products["category"] == cat] for cat in categories}
 
-    fig = go.Figure()
-    for idx, (cat, df) in enumerate(dfs.items()):
-        color_idx = idx % len(color_scale)
-        fig = fig.add_trace(go.Scatter(x=df["x_embeds_tsne"], y=df["y_embeds_tsne"], name=cat, text = df['name'], mode='markers',
-                                    marker=dict(color=color_scale[color_idx]),hovertemplate='%{text}'))
-    fig.update_layout(title='Visualisation of product name embeddings')   
-    st.plotly_chart(fig)
+        # use a color_scale to make sure all categories are ploted in unique colors
+        color_scale = px.colors.qualitative.Light24_r
 
-    st.write('Choose product categories for visualisation:')
-
-
-   # make checkbox for the user to choose, if the data shall be shown
-    if st.checkbox('Show product data'):
-
-        # plot the count of products per category    
-        fig = px.bar(plot_products.groupby('category').name.count()).update_layout(
-            xaxis = {"categoryorder":"total descending"},
-            yaxis_title="Sum of expenses in €",
-            xaxis_title='',
-            showlegend=False,
-            title='Number of products per category at REWE')
+        fig = go.Figure()
+        for idx, (cat, df) in enumerate(dfs.items()):
+            color_idx = idx % len(color_scale)
+            fig = fig.add_trace(go.Scatter(x=df["x_embeds_tsne"], y=df["y_embeds_tsne"], name=cat, text = df['name'], mode='markers',
+                                        marker=dict(color=color_scale[color_idx]),hovertemplate='%{text}'))
+        fig.update_layout(title='REWE assortment Mistral embeddings (1024 dimensions reduced with t-SNE)')   
         st.plotly_chart(fig)
-        # print dataframe and show images of the products in the image column
-        st.dataframe(rewe_products.drop(['x_embeds_tsne','y_embeds_tsne'],axis=1),
-                column_config={
-                    'image':st.column_config.ImageColumn('Product Image'),
-                    'price':st.column_config.NumberColumn('Price', format='%.2f €'),
-                    'name':'Name',
-                    'category':'Category'},
-                    hide_index=True)
+
+
+    if not presentation:
+    # make checkbox for the user to choose, if the data shall be shown
+        if st.checkbox('Show product data'):
+
+            # plot the count of products per category    
+            fig = px.bar(plot_products.groupby('category').name.count()).update_layout(
+                xaxis = {"categoryorder":"total descending"},
+                yaxis_title="Sum of expenses in €",
+                xaxis_title='',
+                showlegend=False,
+                title='Number of products per category at REWE')
+            st.plotly_chart(fig)
+            # print dataframe and show images of the products in the image column
+            st.dataframe(rewe_products.drop(['x_embeds_tsne','y_embeds_tsne'],axis=1),
+                    column_config={
+                        'image':st.column_config.ImageColumn('Product Image'),
+                        'price':st.column_config.NumberColumn('Price', format='%.2f €'),
+                        'name':'Name',
+                        'category':'Category'},
+                        hide_index=True)
+        
